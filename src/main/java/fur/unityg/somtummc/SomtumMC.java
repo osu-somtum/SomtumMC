a9e366baed9d6a7f14e0e00feb6bd33df3528fab
UnityG <furries@duck.com>
2024-05-12 13:26:42 +0000

    adding command and changing plugin.yml...

--- src/main/java/fur/unityg/somtummc/SomtumMC.java
+++ src/main/java/fur/unityg/somtummc/SomtumMC.java
@@ -4,8 +4,11 @@ import fur.unityg.somtummc.Commands.RulesCommand;
 import fur.unityg.somtummc.Discord.DiscCommand;
 import fur.unityg.somtummc.Discord.PlayerEvent;
 import me.lucko.spark.api.Spark;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
@@ -17,12 +20,18 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.awt.*;
+
 public final class SomtumMC extends JavaPlugin implements Listener {
     String token = "MTIzODUyMzc1NTk0NTUyNTMwOQ.GcELdS.E6k52w1TLclAlPGX32iXrxwNWPsjXueeBAzTwo";
-
     private ShardManager shardManager;
     public ShardManager getShardManager() {
         return shardManager;
+    }
+    private JDA jda;
+
+    public JDA getJDA() {
+        return jda;
     }
 
     @Override
@@ -53,6 +62,16 @@ public final class SomtumMC extends JavaPlugin implements Listener {
 
         // Starting Embed
 
+        TextChannel channel = jda.getTextChannelById("1238726409942470749");
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(new Color(64, 176, 56));
+        embed.setAuthor("The server started up succesfully!!");
+        if (channel != null) {
+            channel.sendMessageEmbeds(embed.build()).queue();
+        } else {
+            Bukkit.getServer().getLogger().warning("MOTHA FAKER IRT NO FOUND AHHAHASFHASUIBFAQojasfhjknaefwjkhaefwj g");
+        }
+
         // Registering Slash Command and Minecraft Events
 
         this.getCommand("rules").setExecutor(new RulesCommand());
@@ -64,6 +83,14 @@ public final class SomtumMC extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        TextChannel channel = jda.getTextChannelById("1238726409942470749");
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(new Color(128, 5, 5));
+        embed.setAuthor("The server shutdownned succesfully!!");
+        if (channel != null) {
+            channel.sendMessageEmbeds(embed.build()).queue();
+        } else {
+            Bukkit.getServer().getLogger().warning("MOTHA FAKER IRT NO FOUND AHHAHASFHASUIBFAQojasfhjknaefwjkhaefwj g");
+        }
     }
 }
